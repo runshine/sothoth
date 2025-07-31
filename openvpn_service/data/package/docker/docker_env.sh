180dfa220befe50d4546a4bd9ef83da7da29b193
runshine <woshiyuhao0819@gmail.com>
2025-07-31 15:34:45 +0800

    fixs

--- openvpn_service/data/package/docker/docker_env.sh
+++ openvpn_service/data/package/docker/docker_env.sh
@@ -8,6 +8,6 @@ cat <<EOF > "$INIT_FILE"
 alias docker="${DOCKER_ROOT_DIR}/bin/docker -H 'unix:///${DOCKER_ROOT_DIR}/run/docker.sock'"
 EOF
 # 启动子 shell 并加载初始化文件
-TMOUT=0 HISTSIZE=1000 "${DOCKER_ROOT_DIR}/../utils/bash" --rcfile "$INIT_FILE"
+TMOUT=0 HISTSIZE=1000 PROMPT_COMMAND= "${DOCKER_ROOT_DIR}/../utils/bash" --rcfile "$INIT_FILE"
 # 清理临时文件（可选）
 rm -f "$INIT_FILE"
