bcb8a59e9a6f4568120c4bb3d02675a05596f909
runshine <woshiyuhao0819@gmail.com>
2025-08-13 10:01:13 +0800

    fixs

--- java_utils/attach_helper/agent-attach-java/src/main/java/com/guance/javaagent/JavaAgentLoader.java
+++ java_utils/attach_helper/agent-attach-java/src/main/java/com/guance/javaagent/JavaAgentLoader.java
@@ -32,36 +32,48 @@ public class JavaAgentLoader {
         }
         try {
             List<VirtualMachineDescriptor> list = VirtualMachine.list();
+            if(list.size()==0){
+                logger.warn("Not found any java VirtualMachine");
+            }
             for (int i = 0; i < list.size(); i++) {
                 VirtualMachineDescriptor virtualMachineDescriptor = list.get(i);
                 String pid = virtualMachineDescriptor.id();
-		        logger.info(pid);
-		    //    logger.info(virtualMachineDescriptor.displayName());
+// 		        logger.info(pid);
+// 		        logger.info(virtualMachineDescriptor.displayName());
                 VirtualMachine attach = null;
                 if (idMode && config.getPid().equals(pid) ){
+                     logger.info("Find a match VirtualMachine, pid: " + pid + ", name: " + virtualMachineDescriptor.displayName());
                      attach = VirtualMachine.attach(pid);
                 }
                 if (nameMode && virtualMachineDescriptor.displayName().equals(config.getDisplayName())){
+                    logger.info("Find a match VirtualMachine, pid: " + pid + ", name: " + virtualMachineDescriptor.displayName());
                     attach = VirtualMachine.attach(pid);
                 }
                 if (attach == null){
+                    logger.info("Not match VirtualMachine, pid: " + pid + ", name: " + virtualMachineDescriptor.displayName());
                     continue;
                 }
                 if (config.getAgentJar() != null && !config.getAgentJar().equals("")){
+                    logger.info("try to loading java agent: " +config.getAgentJar() +  ", options: " + config.getOptions());
                     attach.loadAgent(config.getAgentJar(), config.getOptions());
                 }
                  if (config.getAgentSo() != null && !config.getAgentSo().equals("")){
-                    if( config.getOptions() == null ||  config.getOptions().length() == 0 )
+                    if( config.getOptions() == null ||  config.getOptions().length() == 0 ){
+                        logger.info("try to loading native agent: " +config.getAgentSo() + " without options");
                         attach.loadAgentPath(config.getAgentSo());
-                    else
-                    attach.loadAgentPath(config.getAgentSo(), config.getOptions());
+                       }
+                    else{
+                        logger.info("try to loading native agent: " +config.getAgentSo() + ", options: " + config.getOptions() );
+                        attach.loadAgentPath(config.getAgentSo(), config.getOptions());
+                    }
                  }
                 attach.detach();
-                logger.info(String.format("attach agent into [%s]",virtualMachineDescriptor.displayName()));
+                logger.info(String.format("attach agent success into [%s]",virtualMachineDescriptor.displayName()));
                 System.exit(0);
                 return;
             }
         } catch (Exception e) {
+            logger.info(String.format("attach agent failed [%s]",e.toString()));
             throw new RuntimeException(e);
         }
         System.exit(255);
