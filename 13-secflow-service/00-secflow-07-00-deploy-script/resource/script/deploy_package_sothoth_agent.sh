f26315dcae61af506e00dcced64da6c980badc20
UltramanGaia <3213359017@qq.com>
2026-03-06 10:15:33 +0800

    sothothv1

--- 13-secflow-service/00-secflow-07-00-deploy-script/resource/script/deploy_package_sothoth_agent.sh
+++ 13-secflow-service/00-secflow-07-00-deploy-script/resource/script/deploy_package_sothoth_agent.sh
@@ -11,12 +11,15 @@ deploy_package.sh sothothv1_agent
 
 SERVER_IP=$(${ROOT_DIR}/usr/bin/busybox nslookup nginx-server.sothoth.svc.cluster.local 10.96.0.10 2>/dev/null | grep -E '^Address: ' | grep -v '#' | awk '{print $2}' | head -n1)
 
+CMD="${ROOT_DIR}/usr/bin/sothothv1_agent -server=http://${SERVER_IP}:80 -projectId=${PROJECT_ID} -nodeId=${NODE_ID} -gaiasecDir=${ROOT_DIR}/usr/sothoth -autohook"
+echo ${CMD}
+
 cat  <<EOF > ${ROOT_DIR}/service_config/99_sothothv1_service.json
 {
   "name": "sothoth",
   "description": "sothoth v1 service",
-  "start_cmd": "${ROOT_DIR}/usr/bin/sothothv1_agent -server=http://${SERVER_IP}:80 -projectId=${PROJECT_ID} -nodeId=${NODE_ID} -gaiasecDir=${ROOT_DIR}/usr/sothoth -autohook",
-  "pid_file": "${ROOT_DIR}/var/sothoth/nodeagent.pid",
+  "start_cmd": "${CMD}",
+  "pid_file": "${ROOT_DIR}/usr/sothoth/nodeagent.pid",
   "stdout_log": "${ROOT_DIR}/var/log/monitor_sothothv1_stdout.log",
   "stderr_log": "${ROOT_DIR}/var/log/monitor_sothothv1_stderr.log",
   "work_dir": "${ROOT_DIR}/usr/sothoth",
