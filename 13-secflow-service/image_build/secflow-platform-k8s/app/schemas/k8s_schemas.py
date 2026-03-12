e75da8e7b7309b37c28fa1eac06ea02ef1e3a79c
hgh1234567 <2738592406@qq.com>
2026-03-12 09:16:12 +0800

    Revert "修改k8s微服务字段"
    This reverts commit 4c13947c3e19a1e39a3a30b86eb6279ce585fb4c.

--- 13-secflow-service/image_build/secflow-platform-k8s/app/schemas/k8s_schemas.py
+++ 13-secflow-service/image_build/secflow-platform-k8s/app/schemas/k8s_schemas.py
@@ -294,10 +294,11 @@ class DeploymentInfo(BaseModel):
     namespace: str
     label: Dict[str, str] = Field(default_factory=dict)
     annotation: Dict[str, str] = Field(default_factory=dict)
-    replica: int = 0
-    ready_replica: int = 0
-    available_replica: int = 0
-    updated_replica: int = 0
+    replicas: int = 0
+    ready_replicas: int = 0
+    available_replicas: int = 0
+    updated_replicas: int = 0
+    conditions: List[Dict[str, Any]] = Field(default_factory=list)
     selector: Dict[str, str] = Field(default_factory=dict)
     container: List[Dict[str, Any]] = Field(default_factory=list)
     created_at: Optional[str] = None
