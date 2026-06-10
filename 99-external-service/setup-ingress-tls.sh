38bb06b6ad9a1c78afdcbff4bd07ba8a4d5688d7
runshine <447491275@qq.com>
2026-06-10 11:40:54 +0000

    commit

--- 99-external-service/setup-ingress-tls.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${SCRIPT_DIR}/../00-pre-init/setup-k8s-tls-secrets.sh"
-
-setup_tls_secret "*.ai.icsl.huawei.com"             "sothothv2-ns" "wildcard-secflow-tls"
-setup_tls_secret "*.ai.icsl.huawei.com"             "sothoth"      "wildcard-secflow-tls"
-setup_tls_secret "*.ai.icsl.huawei.com"             "secflow-ns"   "wildcard-secflow-tls"
-setup_tls_secret "*.gaiasec.ai.icsl.huawei.com"     "sothoth"      "wildcard-secflow-tls"
