3f06d134d660c132cd8afe92e625e22ea6bd68a8
runshine <447491275@qq.com>
2026-06-13 08:41:11 +0000

    Move auth image build into submodule

--- 13-secflow-service/image_build/secflow-platform-auth/app/__init__.py
+++
@@ -1,1 +0,0 @@
-from app.router import auth, users, roles
--- 13-secflow-service/image_build/secflow-platform-auth/app/router/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Router路由汇总"""
-
-from app.router.auth import router as auth_router
-from app.router.users import router as users_router
-from app.router.roles import router as role_router
-from app.router.machine_tokens import router as machine_tokens_router
-from app.router.org import router as org_router
